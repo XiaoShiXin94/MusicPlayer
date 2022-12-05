913138287c9a1f742c910dbf4e3b76631b419d96
shixin <11414393+shixin94@user.noreply.gitee.com>
2022-12-05 09:28:28 +0000

    第一次上传
    Signed-off-by: shixin <11414393+shixin94@user.noreply.gitee.com>

---
+++ Music/Music/Songs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music
+{
+    public partial class Songs
+    {
+        public string msg { get; set; }
+        public string code { get; set; }
+        public string songId { get; set; }
+        public string songName { get; set; }
+        public string songUrl { get; set; }
+        public string songType { get; set; }
+        //public string songTime { get; set; }
+        public string singer { get; set; }
+        public string picture { get; set; }
+        
+    }
+}
---
+++ Music/Music/SongsJson.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music
+{
+    class SongsJson
+    {
+        public string msg { get; set; }
+        public string code { get; set; }
+        public struct data
+        {
+            public string songId { get; set; }
+            public string songName { get; set; }
+            public string songUrl { get; set; }
+            public string singer { get; set; }
+            public string picture { get; set; }
+        }
+        public data songsData;
+    }
+}
